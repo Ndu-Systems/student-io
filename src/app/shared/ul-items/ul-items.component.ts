89fabc8b4a99263b5bf67dae5b0d91fe7838bcea
NdumisoMthembu <mrnnmthembu@gmail.com>
2019-01-05 20:59:25 +0200

    login

--- src/app/shared/ul-items/ul-items.component.ts
+++ src/app/shared/ul-items/ul-items.component.ts
@@ -1,11 +1,11 @@
-import { USER_TOKEN } from './../config';
-import {  UserToken } from './../../model/token/token.model';
-import { User } from 'src/app/model';
-import { AccountService } from 'src/app/services/service/account.service';
-import { LoginModel } from './../../models/login.model';
+import { USER_TOKEN } from "./../config";
+import { UserToken } from "./../../model/token/token.model";
+import { User } from "src/app/model";
+import { AccountService } from "src/app/services/service/account.service";
+import { LoginModel } from "./../../models/login.model";
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 import { Component, OnInit } from "@angular/core";
-import { Router } from '@angular/router';
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-ul-items",
@@ -17,7 +17,11 @@ export class UlItemsComponent implements OnInit {
   formGroup: FormGroup;
   username: any;
   password: any;
-  constructor(private formBuilder: FormBuilder, private acountService:AccountService, private router:Router) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private acountService: AccountService,
+    private router: Router
+  ) {
     this.formGroup = formBuilder.group({
       username: [this.username, Validators.required],
       password: [this.password, Validators.required]
@@ -33,19 +37,19 @@ export class UlItemsComponent implements OnInit {
     if (!this.showLogin) main.className = "main";
     console.log(main);
   }
-  onLogin(user:User) { 
-    let u:User = {
+  onLogin(user: User) {
+    let u: User = {
       Email: "malesuada.vel.venenatis@loremeget.com",
       Password: "OHL83WJC0ND"
-    }
-   this.acountService.Login(u).subscribe(response=>{
-    let userResponse:any = response;
-    if(!userResponse.value){
-      return false;
-    }
-    let token = userResponse.value.token;
-     localStorage.setItem(USER_TOKEN,JSON.stringify(token));
-      this.router.navigate(['dashboard'])
-   });
+    };
+    this.acountService.Login(u).subscribe(response => {
+      let userResponse: any = response;
+      if (!userResponse.value) {
+        return false;
+      }
+      let token = userResponse.value.token;
+      localStorage.setItem(USER_TOKEN, JSON.stringify(token));
+      this.router.navigate(["dashboard"]);
+    });
   }
 }
