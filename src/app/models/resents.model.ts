935262cc4b8fea15f9ec9bb279fdc353299d9be0
NdumisoMthembu <mrnnmthembu@gmail.com>
2019-01-06 02:15:58 +0200

    Dashboard

---
+++ src/app/models/resents.model.ts
@@ -0,0 +1,5 @@
+export interface Recents {
+  discription:string;
+  value_1:string;
+  value_2:string;
+}
--- src/app/views/dashboard/cards/card-med/card-med.component.ts
+++ src/app/views/dashboard/cards/card-med/card-med.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recents } from 'src/app/models/resents.model';
 
 @Component({
   selector: 'app-card-med',
@@ -9,9 +10,18 @@ export class CardMedComponent implements OnInit {
   heading="Recently registered";
   count="305";
   image="group-rkP1DGAWN.svg";
+  items:Array<Recents> = [
+    {
+      discription:'Nomfundo Masbongo',
+      value_1:'IT:AI',
+      value_2:'IT:SD'
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+
+
