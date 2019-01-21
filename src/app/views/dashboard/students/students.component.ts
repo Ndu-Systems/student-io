ceb9802b39cd100c63bb442441aa56bac86f95c5
NdumisoMthembu <mrnnmthembu@gmail.com>
2019-01-21 11:41:25 +0200

    student table design

--- src/app/views/dashboard/students/students.component.ts
+++ src/app/views/dashboard/students/students.component.ts
@@ -7,53 +7,56 @@ import { Component, OnInit } from "@angular/core";
 })
 export class StudentsComponent implements OnInit {
   students = [
-   
-  
     {
       name: "Jonn Nkosi",
       email: "MusaNkosi1@gmail.com",
-      studentNumber:`2678628`,
-      cell:'071454545',
-      course:'IT',
-      year:2018,
-      status:'pending'
+      studentNumber: `2678628`,
+      cell: "071454545",
+      course: "IT",
+      year: 2018,
+      status: "pending",
+      subjects: 3
     },
     {
       name: "Jonn Nkosi",
       email: "MusaNkosi1@gmail.com",
-      studentNumber:`2678628`,
-      cell:'071454545',
-      course:'IT',
-      year:2018,
-      status:'active'
+      studentNumber: `2678628`,
+      cell: "071454545",
+      course: "IT",
+      year: 2018,
+      status: "active",
+      subjects: 3
     },
     {
       name: "Jonn Nkosi",
       email: "MusaNkosi1@gmail.com",
-      studentNumber:`2678628`,
-      cell:'071454545',
-      course:'IT',
-      year:2018,
-      status:'overdue'
+      studentNumber: `2678628`,
+      cell: "071454545",
+      course: "IT",
+      year: 2018,
+      status: "overdue",
+      subjects: 3
     },
     {
       name: "Jonn Nkosi",
       email: "MusaNkosi1@gmail.com",
-      studentNumber:`2678628`,
-      cell:'071454545',
-      course:'IT',
-      year:2018,
-      status:'pending'
+      studentNumber: `2678628`,
+      cell: "071454545",
+      course: "IT",
+      year: 2018,
+      status: "pending",
+      subjects: 3
     },
     {
       name: "Jonn Nkosi",
       email: "MusaNkosi1@gmail.com",
-      studentNumber:`2678628`,
-      cell:'071454545',
-      course:'IT',
-      year:2018,
-      status:'active'
-    },
+      studentNumber: `2678628`,
+      cell: "071454545",
+      course: "IT",
+      year: 2018,
+      status: "active",
+      subjects: 3
+    }
   ];
   constructor() {}
 
